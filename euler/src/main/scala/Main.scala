cce1f679e136d264077dbf3adec8cf1e2a76c68b
Matt March <matthewmarch29@gmail.com>
2020-05-13 16:37:39 +0100

    Problem 2 completed

--- euler/src/main/scala/Main.scala
+++ euler/src/main/scala/Main.scala
@@ -1,3 +1,5 @@
+import scala.collection.mutable.ArrayBuffer
+
 object Main extends App {
 
   if (args.size == 0)
@@ -6,6 +8,7 @@ object Main extends App {
     println(
     args(0) match {
       case "1" => Problem1
+      case "2" => Problem2
       case _ => "Not solved yet!"
     }
   )
@@ -14,5 +17,16 @@ object Main extends App {
     val range = (1 to 999).toList
     val multiples = range.filter(i => i % 3 == 0 || i % 5 == 0)
     multiples.reduce(_ + _)
+  }
+
+  def Problem2 = {
+    val limit = 4000000
+    val fibonacci = ArrayBuffer(0, 1)
+    while (fibonacci.head <= limit) {
+      val nextNumber = fibonacci.take(2).reduce(_ + _)
+      fibonacci.prepend(nextNumber)
+    }
+    fibonacci.trimEnd(1)
+    fibonacci.filter(_ % 2 == 0).reduce(_ + _)
   }
 }
