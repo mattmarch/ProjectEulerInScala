faab00ab6f907696c840ee68d4c46140ab527907
Matt March <matthewmarch29@gmail.com>
2020-05-13 16:14:23 +0100

    Use CLI argument to choose question to solve

--- euler/src/main/scala/Main.scala
+++ euler/src/main/scala/Main.scala
@@ -1,5 +1,18 @@
 object Main extends App {
-  val multiples = (1 to 999).toList.filter(i => i % 3 == 0 || i % 5 == 0)
-  val sum = multiples.reduce(_ + _)
-  println(sum)
+
+  if (args.size == 0)
+    println("Specify a question number!")
+  else
+    println(
+    args(0) match {
+      case "1" => Problem1
+      case _ => "Not solved yet!"
+    }
+  )
+
+  def Problem1 = {
+    val range = (1 to 999).toList
+    val multiples = range.filter(i => i % 3 == 0 || i % 5 == 0)
+    multiples.reduce(_ + _)
+  }
 }
